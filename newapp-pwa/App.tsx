1ca39f1102893b6f0a8822020cd8bb36fdfa4465
Karol Etrych <karol.etrych@gmail.com>
2020-04-15 18:41:04 +0200

    setup connection to firestore

--- newapp-pwa/App.tsx
+++ newapp-pwa/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as firebase from 'firebase';
+import '@firebase/firestore';
 import Constants from 'expo-constants';
 
 let firebaseConfig = Constants.manifest.extra.firebaseConfig;
 firebase.initializeApp(firebaseConfig);
-firebase.database().ref('users/1').set({
-  highscore: 42
-});
+
+const dbh = firebase.firestore();
+dbh.collection("characters").doc("mario").set({
+  employment: "plumber",
+  outfitColor: "red",
+  specialAttack: "fireball"
+})
 
 
 export default function App() {
